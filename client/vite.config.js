56ab73b2f454a04654a2400b5006f14c83dd7ad7
Puritized <pecamoint@gmail.com>
2025-11-15 15:35:16 +0100

    Week 7 Assignment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/deployment-and-devops-essentials-Puritized/', // ← your repo name
   plugins: [react()],
-  base: '/deployment-and-devops-essentials-Puritized/',
 });
